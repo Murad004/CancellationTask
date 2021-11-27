899ef33a6803382ca3f9a0224b6045fa96f745ff
Sadi_er59 <Sadi_er59@STHQ0124-16>
2021-11-27 14:50:17 +0400

    Add project files.

---
+++ CancellationTask/Model/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancellationTask.Model
+{
+    public class Document
+    {
+
+        public static string GetText()
+        {
+            return @"Salam Dunya";
+
+        }
+    }
+}
